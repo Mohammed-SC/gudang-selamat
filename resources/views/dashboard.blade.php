fab408c0564ce09b4736bcdc3ba27c94e07d7cd5
Mohef-beep <hendra.mohammed@gmail.com>
2025-03-24 16:47:14 +0700

    Penambahan dashboard: card laporan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,12 +21,52 @@
 
 <section class="content">
     <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info elevation-1"><i class="fas fa-boxes"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Total Barang</span>
+                        <span class="info-box-number">{{ \App\Models\NamaBarang::count() }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-warehouse"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Total Stok</span>
+                        <span class="info-box-number">{{ \App\Models\Stock::sum('jumlah') }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                    <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-sign-in-alt"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Barang Masuk</span>
+                        <span class="info-box-number">{{ \App\Models\BarangMasuk::count() }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-sign-out-alt"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Barang Keluar</span>
+                        <span class="info-box-number">{{ \App\Models\BarangKeluar::count() }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h3>Selamat Datang!</h3>
-                        <p>Halo, {{ session('user')->name }}! Ini adalah dashboard utama.</p>
+                        <h3>Selamat Datang! Mas Arjuna</h3>
+                        <p>Halo, {{ session('user')->name }}! Semoga Succes kedepan!!!.</p>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,10 +24,10 @@
 
 <body class="hold-transition sidebar-mini layout-fixed layout-footer-fixed">
     <div class="wrapper">
-        <!-- Preloader -->
+        <!-- Preloader
         <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-                height="60" width="60">
+                height="60" width="60"> -->
         </div>
         <!-- Navbar -->
         @include('layouts.header')
